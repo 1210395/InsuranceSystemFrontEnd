160549412c2d4e68dd3a9b0881382d5d2b9e39f3
Adam Khabisa <101061000+methan6427@users.noreply.github.com>
2026-02-28 01:33:55 +0200

    Fix import case for LabRequestList component

--- src/Component/Lab/LabDashboard.jsx
+++ src/Component/Lab/LabDashboard.jsx
@@ -9,7 +9,7 @@ import logger from "../../utils/logger";
 
 import LabSidebar from "./LabSidebar";
 import LabHeader from "./LabHeader";
-import LabRequestList from "./LabRequestList";
+import LabRequestList from "./labrequestlist";
 import LabProfile from "../Profile/LabProfile";
 import NotificationsList from "../Notification/NotificationsList";
 import LogoutDialog from "../Auth/LogoutDialog";
